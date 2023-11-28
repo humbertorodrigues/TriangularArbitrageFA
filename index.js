968764be879c66d90032470dd5633be464e79942
blbraga@gmail.com <blbraga@gmail.com>
2023-11-28 09:27:46 -0300

    Ws e html.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const WebSocket = require('ws');
+var TradingCore = require('./lib/TradingCore');
+var DBHelpers = require('./lib/DBHelpers').DBHelpers;
+var PairRanker = require('./lib/PairRanker').PairRanker;
+
+const wss = new WebSocket.Server({ port: 8080 });
+
 // load logger library
 const logger = require('./lib/LoggerCore');
 
@@ -30,46 +37,94 @@ if (process.env.activeExchange == 'binance'){
   exchangeAPI.WS = new api.BinanceWS(beautifyResponse);
 }
 
-var botOptions = {
-    UI: {
-      title: 'Top Potential Arbitrage Triplets, via: ' + process.env.binanceColumns
-    },
-    arbitrage: {
-      paths: process.env.binanceColumns.split(','),
-      start: process.env.binanceStartingPoint
-    },
-    storage: {
-      logHistory: false
-    },
-    trading: {
-      paperOnly: true,
-      // only candidates with over x% gain potential are queued for trading
-      minQueuePercentageThreshold: 3,
-      // how many times we need to see the same opportunity before deciding to act on it
-      minHitsThreshold: 5
+wss.on('connection', function connection(ws) {
+  ws.on('message', function message(data) {
+    try {
+      var json = JSON.parse(data);
+      console.log(json);
+      this.dbHelpers = new DBHelpers();
+      this.pairRanker = new PairRanker();
+
+      var ctrl = {
+        options: {
+          UI: {
+            title: 'Top Potential Arbitrage Triplets, via: ' + process.env.binanceColumns
+          },
+          arbitrage: {
+            paths: json.binanceColumns.split(','),
+            start: json.binanceStartingPoint
+          },
+          storage: {
+            logHistory: false
+          },
+          trading: {
+            paperOnly: true,
+            // only candidates with over x% gain potential are queued for trading
+            minQueuePercentageThreshold: 3,
+            // how many times we need to see the same opportunity before deciding to act on it
+            minHitsThreshold: 5
+          }
+        },
+        storage: {
+          trading: {
+          // queued triplets
+            queue: [],
+            // actively trading triplets
+            active: []
+          },
+          candidates: [],
+          streams: [],
+          pairRanks: []
+        },
+        logger: logger,
+        exchange: exchangeAPI
+      };
+
+      // every pingback from the websocket(s)
+      ctrl.storage.streamTick = (stream, streamID) => {
+        ctrl.storage.streams[streamID] = stream;
+
+        if (streamID == 'allMarketTickers') {
+          // Run logic to check for arbitrage opportunities
+          ctrl.storage.candidates = ctrl.currencyCore.getDynamicCandidatesFromStream(stream, ctrl.options.arbitrage);
+
+          // Run logic to check for each pairs ranking
+          var pairToTrade = this.pairRanker.getPairRanking(ctrl.storage.candidates, ctrl.storage.pairRanks, ctrl, ctrl.logger);
+          if (pairToTrade != 'none') {
+            // console.log("<----GO TRADE---->");
+          }
+
+          // queue potential trades
+          if (this.tradingCore)
+            this.tradingCore.updateCandidateQueue(stream, ctrl.storage.candidates, ctrl.storage.trading.queue);
+
+          ws.send(JSON.stringify(ctrl.storage.candidates));
+          // update UI with latest values per currency
+          //ctrl.UI.updateArbitageOpportunities(ctrl.storage.candidates);
+
+          if (ctrl.options.storage.logHistory) {
+            // Log arbitrage data to DB, if enabled
+            this.dbHelpers.saveArbRows(ctrl.storage.candidates, ctrl.storage.db, ctrl.logger);
+            this.dbHelpers.saveRawTick(stream.arr, ctrl.storage.db, ctrl.logger);
+          }
+
+        }
+      }
+
+      // loading the CurrencyCore starts the streams
+      ctrl.logger.info('--- Starting Currency Streams');
+      ctrl.currencyCore = require('./lib/CurrencyCore')(ctrl);
+
+      this.tradingCore = TradingCore(ctrl.options.trading, ctrl.currencyCore);
+      // use this for callbacks for ongoing trade workflows
+      // this.tradingCore.on('queueUpdated', (queue, timeStarted)=>{  });
+      // this.tradingCore.on('newTradeQueued', (candidate, timeStarted)=>{  });
+    } catch (error) {
+      console.error(error);
+      ws.send('{"error":"'+error+'"}');
+      // Expected output: ReferenceError: nonExistentFunction is not defined
+      // (Note: the exact output may be browser-dependent)
     }
-  },
-  ctrl = {
-    options: botOptions,
-    storage: {
-      trading: {
-      // queued triplets
-        queue: [],
-        // actively trading triplets
-        active: []
-      },
-      candidates: [],
-      streams: [],
-      pairRanks: []
-    },
-    logger: logger,
-    exchange: exchangeAPI
-  };
+  });
 
-ctrl.UI       = require('./lib/UI')(ctrl.options),
-ctrl.events   = require('./lib/EventsCore')(ctrl);
-
-// We're ready to start. Load up the webhook streams and start making it rain.
-require('./lib/BotCore')(ctrl);
-
-ctrl.logger.info('----- Bot Startup Finished -----');
+});
